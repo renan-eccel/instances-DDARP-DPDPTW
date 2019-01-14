ce47748b2ccecc7d200a6485640bf592f8adb698
renan-eccel <renan.eccel@gmail.com>
2019-01-14 09:34:03 -0200

    add coment

--- dinamizacao/analyses/analyse.py
+++ dinamizacao/analyses/analyse.py
@@ -29,31 +29,76 @@ hdf = (
 )
 
 last_values = None
-interarrival_gap_by_index = {}
+info_by_index = {'interarrival_gap': {},
+                 'perfect_interarrival_gap': {}
+                 }
 for values in hdf.itertuples():
-    if values.interarrival < values.perfect_interarrival:
+    if str(values.interarrival) == 'nan':
+        # Case nan
+        info_by_index.get('interarrival_gap')[values.Index] = None
+        info_by_index.get('perfect_interarrival_gap')[values.Index] = None
+
+    elif values.interarrival < values.perfect_interarrival:
         if last_values is None:
-            # Case 1
-            interarrival_gap_by_index[values.Index] = \
+            # Case 1, first row of the instance (i==0)
+            info_by_index.get('interarrival_gap')[values.Index] = \
                 values.perfect_interarrival - values.interarrival
+
+            info_by_index.get('perfect_interarrival_gap')[values.Index] = \
+                values.perfect_interarrival
+
         elif str(last_values.interarrival) == 'nan':
-            # Case 2
-            interarrival_gap_by_index[values.Index] = \
+            # Case 1, other rows (i==0)
+            info_by_index.get('interarrival_gap')[values.Index] = \
                 values.perfect_interarrival - values.interarrival
+
+            info_by_index.get('perfect_interarrival_gap')[values.Index] = \
+                values.perfect_interarrival
+
         else:
-            # Case 3
-            interarrival_gap_by_index[values.Index] = \
+            # Case 2 (i>0)
+            info_by_index.get('interarrival_gap')[values.Index] = \
                 values.perfect_interarrival - values.interarrival \
                 + ((values.perfect_interarrival - values.interarrival)
                     / values.perfect_interarrival) \
-                * interarrival_gap_by_index.get(last_values.Index)
+                * info_by_index.get('interarrival_gap').get(last_values.Index)
+
+            info_by_index.get('perfect_interarrival_gap')[values.Index] = \
+                values.perfect_interarrival \
+                + ((values.perfect_interarrival - values.interarrival)
+                    / values.perfect_interarrival) \
+                * info_by_index.get('interarrival_gap').get(last_values.Index)
 
     else:
-        # Case 0
-        interarrival_gap_by_index[values.Index] = 0
+        # Case 3, otherwise
+        info_by_index.get('interarrival_gap')[values.Index] = 0
+        info_by_index.get('perfect_interarrival_gap')[values.Index] = \
+            values.perfect_interarrival
     last_values = values
 
 # check if all rows of the dataframe resulted in an interarrival_deviation
-assert hdf.shape[0] == len(interarrival_gap_by_index)
+assert hdf.shape[0] == len(info_by_index.get('interarrival_gap'))
+assert hdf.shape[0] == len(info_by_index.get('perfect_interarrival_gap'))
 
-interarrival_gap = pd.Series(interarrival_gap_by_index)
+interarrival_gap = pd.Series(info_by_index.get('interarrival_gap'))
+perfect_interarrival_gap = pd.Series(info_by_index.get(
+    'perfect_interarrival_gap'))
+hdf = (
+    hdf.assign(interarrival_gap=interarrival_gap)
+       .assign(perfect_interarrival_gap=perfect_interarrival_gap)
+       .assign(dynamism=lambda x: 1
+               - x.groupby(columns_to_group).interarrival_gap.sum()
+               / x.groupby(columns_to_group).perfect_interarrival_gap.sum())
+)
+
+# check if correlation between perfect_interval and perfect_interval_gap is
+# valid
+mask = (hdf.interarrival_gap
+        + hdf.perfect_interarrival
+        - hdf.perfect_interarrival_gap
+        < 0.01) | \
+       (hdf.interarrival_gap
+        + hdf.interarrival
+        - hdf.perfect_interarrival_gap
+        < 0.01)
+assert hdf[~mask].shape[0] == df_grouped.id.count().shape[0]
