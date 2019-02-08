9a1ad9ef472548b5abe9e0c9ded4d211685fffb4
renan-eccel <renan.eccel@gmail.com>
2019-02-08 12:43:00 -0200

    fix number of requests

--- dinamizacao/analysis/test_poisson.py
+++ dinamizacao/analysis/test_poisson.py
@@ -3,7 +3,7 @@ import numpy as np
 import analysis_tools
 
 
-def create_df_poisson(number_of_instances, number_of_requests,
+def create_df_poisson(number_of_instances, number_of_requests_desired,
                       planing_horizon):
     '''
     Create a poisson bechmark for the a generic DVRP
@@ -24,26 +24,29 @@ def create_df_poisson(number_of_instances, number_of_requests,
                 last_interarrival = new_interarrival
         return df.assign(arrival_time=arrival_time_list)
 
-    lambda_poisson = number_of_requests / planing_horizon
+    lambda_poisson = number_of_requests_desired / planing_horizon
 
-    exp_beta = lambda_poisson
+    exp_beta = 1 / lambda_poisson
 
-    id_list = [1 + i for i in range(number_of_requests)] * number_of_instances
+    id_list = (
+        [1 + i for i in range(number_of_requests_desired)]
+        * number_of_instances
+    )
 
     instance_list = (
-        [1 + int(i/number_of_requests)
-         for i in range(number_of_instances * number_of_requests)]
+        [1 + int(i/number_of_requests_desired)
+         for i in range(number_of_instances * number_of_requests_desired)]
     )
     array_interarrivals = (
         np.random.exponential(exp_beta,
-                              size=number_of_instances * number_of_requests)
+                              size=number_of_instances
+                              * number_of_requests_desired)
     )
     data = {
         'problem': 'gdvrp',
         'benchmark': 'poisson_test',
         'instance': instance_list,
         'id': id_list,
-        'number_of_requests': number_of_requests,
         'planing_horizon': planing_horizon,
         'interarrival': list(array_interarrivals)
     }
@@ -51,7 +54,12 @@ def create_df_poisson(number_of_instances, number_of_requests,
     df = (
         pd.DataFrame(data)
           .pipe(build_arrival_time_column)
+
           .loc[lambda x: x.arrival_time <= planing_horizon]
+          .assign(number_of_requests=lambda x:
+                  x.groupby(['problem', 'benchmark', 'instance'],
+                            as_index=False).arrival_time.transform('count'))
+          .loc[lambda x: x.number_of_requests == number_of_requests_desired]
           .pipe(analysis_tools.calculate_dynamism, 'planing_horizon')
     )
     return df
