00d738829c94a4e29fe91dbe04c95938b040c8b9
Renan Eccel <renan.eccel@wplex.com.br>
2020-05-07 11:22:50 -0300

    add alpha to berbeglia dinamizator

--- instances/dinamizators/dinamizators.py
+++ instances/dinamizators/dinamizators.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import math
 
 
 def calculate_travel_time(pickup_location_x_coord,
@@ -11,16 +11,18 @@ def calculate_travel_time(pickup_location_x_coord,
     '''
     euclidian_distance = (
         (
-         (delivery_location_x_coord - pickup_location_x_coord)**2
-         + (delivery_location_y_coord - pickup_location_y_coord)**2
-        )
-        ** (0.5))
+            (delivery_location_x_coord - pickup_location_x_coord)**2
+            + (delivery_location_y_coord - pickup_location_y_coord)**2
+        ) ** (0.5)
+    )
 
     return np.ceil(euclidian_distance)
 
 
-def elementwise_min(vector_a, vector_b):
-    return pd.concat([vector_a, vector_b], axis=1).min(axis=1)
+def elementwise_min(x, y):
+    x = x.copy()
+    x[x > y] = y
+    return x
 
 
 def dinamize_as_berbeglia(pickup_location_x_coord,
@@ -30,6 +32,7 @@ def dinamize_as_berbeglia(pickup_location_x_coord,
                           pickup_upper_tw,
                           delivery_upper_tw,
                           pickup_service_time,
+                          alpha,
                           beta):
     travel_time = calculate_travel_time(pickup_location_x_coord,
                                         pickup_location_y_coord,
@@ -39,4 +42,10 @@ def dinamize_as_berbeglia(pickup_location_x_coord,
                                delivery_upper_tw
                                - travel_time
                                - pickup_service_time)
-    return a_latest - beta
+    arrival_time = a_latest.subtract(beta).rename('arrival_time')
+    static_requests = (
+        arrival_time.sample(round(alpha * arrival_time.size))
+    )
+    arrival_time[static_requests.index] = 0
+
+    return arrival_time
