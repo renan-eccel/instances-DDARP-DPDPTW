38fa9f5f4a758270d4f7f66390c268a410e4919b
Renan Eccel <renan.eccel@wplex.com.br>
2020-06-29 18:48:20 -0300

    create first draft for tables

--- instances/transportes_2020/transportes_2020.py
+++ instances/transportes_2020/transportes_2020.py
@@ -1,8 +1,14 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import instances.analysis.dinamize_all as dinamize_all
 import instances.analysis.analyse as analyse
+
+
+def weighted_hist(x, weights, **kwargs):
+    plt.hist(x, weights=weights, **kwargs)
+
 
 sns.set(style='ticks', font="Times New Roman", font_scale=1.35)
 
@@ -20,17 +26,7 @@ df_dyn_urg_req = (
         columns_to_group,
         perfect_interarrival_parameter
     )
-)
-df_dyn_urg_ins = (
-    df_dyn_urg_req
     .reset_index()
-    .assign(
-        urgency_mean_norm_max=lambda x:
-        x.urgency_mean
-        / x.groupby(['benchmark']).urgency_mean.transform('max')
-    )
-    .groupby(['problem', 'benchmark', 'dinamizator',
-              'instance']).max().reset_index()
     .assign(
         cite_benchmark=lambda x:
         x.benchmark.map(
@@ -44,8 +40,26 @@ df_dyn_urg_ins = (
              'fabri_rencht_2006': 'Fabri and Recht (2006)',
              'pankratz_2005': 'Pankratz (2005)',
              'pureza_laporte_2008': 'Pureza and Laporte (2008)'}
-        )
+        ),
+        real_pickup_lower_tw=lambda x:
+            x.loc[:, ['arrival_time', 'pickup_lower_tw']].max(axis=1),
+        real_pltw_norm_h=lambda x:
+            x.real_pickup_lower_tw / x.planing_horizon,
+        arrival_time_norm_h=lambda x:
+            x.arrival_time / x.planing_horizon,
+        weight=lambda x:
+            1 / x.groupby(['problem', 'benchmark']).id.transform('count')
     )
+)
+df_dyn_urg_ins = (
+    df_dyn_urg_req
+    .assign(
+        urgency_mean_norm_max=lambda x:
+        x.urgency_mean
+        / x.groupby(['benchmark']).urgency_mean.transform('max')
+    )
+    .groupby(['problem', 'benchmark', 'dinamizator',
+              'instance']).max().reset_index()
 )
 
 g = sns.FacetGrid(df_dyn_urg_ins,
@@ -94,3 +108,56 @@ plt.xlim(0, 1)
 plt.tight_layout()
 plt.savefig('instances/transportes_2020/fig/dynamism_boxplot_2.png')
 plt.close()
+
+g = sns.FacetGrid(df_dyn_urg_req,
+                  col='cite_benchmark',
+                  hue='cite_benchmark',
+                  palette='colorblind',
+                  height=2.5,
+                  aspect=1.5,
+                  xlim=(-0.05, 1),
+                  despine=False,)
+g.map(weighted_hist,
+      'real_pltw_norm_h',
+      'weight',
+      bins=np.arange(0, 1, 0.1))
+g.set_titles('{col_name}')
+g.set_xlabels('Normalized time windows\n'
+              + 'lower limit')
+g.set_ylabels('Frequency')
+plt.tight_layout()
+plt.savefig('instances/transportes_2020/fig/pickup_lower_tw_hist.png')
+plt.close()
+
+df_corr = (
+    df_dyn_urg_req
+    .groupby(['cite_benchmark', 'cite_dinamizator'])
+    [['arrival_time_norm_h', 'real_pltw_norm_h']]
+    .corr()
+    .drop('arrival_time_norm_h', axis=1)
+    .loc[pd.IndexSlice[:, :, ['arrival_time_norm_h']], :]
+    .droplevel(2)
+    .rename({'real_pltw_norm_h': 'r'}, axis=1)
+    .round(2)
+)
+df_corr.to_csv('instances/transportes_2020/fig/arrival_time_x_pltw_corr.csv')
+
+df_static_requests_per = (
+    (
+        df_dyn_urg_req
+        [df_dyn_urg_req.arrival_time == 0]
+        .groupby(['cite_benchmark', 'cite_dinamizator'])
+        .arrival_time
+        .count()
+        /
+        df_dyn_urg_req
+        .groupby(['cite_benchmark', 'cite_dinamizator'])
+        .arrival_time
+        .count()
+    )
+    .fillna(0)
+    .round(3)
+    .multiply(100)
+)
+df_static_requests_per.to_csv('instances/transportes_2020/fig/'
+                              + 'df_static_requests_per.csv')
